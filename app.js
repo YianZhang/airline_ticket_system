fba5e7abe04aa6112cca89a6632a5ced259fc942
Yian Zhang <yian.zhang@nyu.edu>
2020-12-03 17:28:10 +0800

    basic web se web server built

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ require('./db');
 
 const express = require('express');
 const session = require('express-session');
-const bcrypt = requrie('bcrypt');
+const bcrypt = require('bcrypt');
+//const path = require('path');
 
 const app = express();
 app.userSessions = {}
 
 app.set('view engine','hbs');
+
+const sessionOptions = { 
+	secret: 'secret for signing session id', 
+	saveUninitialized: true, 
+    resave: false, 
+};
+
+app.use(session(sessionOptions));
+app.use(express.urlencoded({extended:false}));
+//app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', (req,res)=>{
+    res.render('index');
+})
+
+app.listen(3000);
+console.log('running');
