1a8766133af06f9e96c2c353b939f42bb194b4e7
Yian Zhang <yian.zhang@nyu.edu>
2020-12-03 21:48:59 +0800

    login/register toy examples

--- app.js
+++ app.js
@@ -18,10 +18,32 @@ const sessionOptions = {
 
 app.use(session(sessionOptions));
 app.use(express.urlencoded({extended:false}));
+app.use((req,res,next)=>{
+    console.log(req.path);
+    next();
+})
 //app.use(express.static(path.join(__dirname,'./public')));
 
 app.get('/', (req,res)=>{
     res.render('index');
+})
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+app.get('/register',(req,res)=>{
+    res.render('register');
+})
+//toy
+const namePassword = {};
+
+//toy
+app.post('/login',(req,res)=>{
+    console.log(req.body.name, req.body.password,req.body.type);
+})
+
+//toy
+app.post('/register',(req,res)=>{
+    console.log(req.body.type);
 })
 
 app.listen(3000);
