fc6e1b105eac88781fba6396b771ff3877fe5dfb
Yian Zhang <yian.zhang@nyu.edu>
2020-12-15 00:56:41 +0800

    one and a half page finished

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ require('./db');
 const express = require('express');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
-//const path = require('path');
+const path = require('path');
 
 const app = express();
+//console.log(path.join(__dirname,'./public'));
+
 app.userSessions = {}
 
 app.set('view engine','hbs');
@@ -23,16 +25,20 @@ app.use((req,res,next)=>{
     next();
 })
 //app.use(express.static(path.join(__dirname,'./public')));
+app.use(express.static(path.join(__dirname,'./public')));
 
 app.get('/', (req,res)=>{
-    res.render('index');
+    res.sendFile(path.join(__dirname+'/public/home_page.html'));
 })
+
+
+/*
 app.get('/login',(req,res)=>{
     res.render('login');
 })
 app.get('/register',(req,res)=>{
     res.render('register');
-})
+})*/
 //toy
 const namePassword = {};
 
@@ -44,6 +50,36 @@ app.post('/login',(req,res)=>{
 //toy
 app.post('/register',(req,res)=>{
     console.log(req.body.type);
+    res.render('register_'+req.body.type);
+})
+
+app.post('/register_Customer',(req,res)=>{
+    //db insert
+    console.log(req.body);
+    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+})
+
+app.post('/register_BookingAgent',(req,res)=>{
+    //db insert
+    console.log(req.body);
+    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+})
+
+app.post('/register_AirlineStaff',(req,res)=>{
+    //db insert
+    console.log(req.body);
+    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    if (req.body.action==='search_for_upcoming_flights'){
+        //db query
+        res.render('home_search_upcoming',{content:'test'});
+    }else if (req.body.action==='check_flight_status'){
+        //db query
+        res.render('home_check_flight',{content:'test'});
+    } 
 })
 
 app.listen(3000);
