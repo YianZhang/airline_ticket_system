29e09bb07d742d971a833a8f5f135f0546f9897e
Yian Zhang <yian.zhang@nyu.edu>
2020-12-17 13:11:32 +0800

    more updates

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use((req,res,next)=>{
     console.log(req.path);
     next();
 })
-//app.use(express.static(path.join(__dirname,'./public')));
-app.use(express.static(path.join(__dirname,'./public')));
+
+app.use(express.static(path.join(__dirname, 'public'),{index:false,extensions:['html']}));
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+    res.redirect('home_page');
 })
 
 
@@ -42,35 +42,48 @@ app.get('/register',(req,res)=>{
 //toy
 const namePassword = {};
 
+//GENERAL
 //toy
 app.post('/login',(req,res)=>{
+    //todo db query and cache data
     console.log(req.body.name, req.body.password,req.body.type);
+    res.redirect('user_home_'+req.body.type);
 })
 
+app.get('/logout',(req,res)=>{
+    //todo: cookies
+    console.log('logged out');
+    res.redirect('/login');
+})
+
+//REGISTER
 //toy
 app.post('/register',(req,res)=>{
     console.log(req.body.type);
     res.render('register_'+req.body.type);
 })
 
-app.post('/register_Customer',(req,res)=>{
+app.post('/register_customer',(req,res)=>{
     //db insert
     console.log(req.body);
-    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+    res.redirect('/');
+    //res.sendFile(path.join(__dirname, '/public/home_page.html'));
 })
 
-app.post('/register_BookingAgent',(req,res)=>{
+app.post('/register_booking_agent',(req,res)=>{
     //db insert
     console.log(req.body);
-    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+    //res.sendFile(path.join(__dirname, '/public/home_page.html'));
 })
 
-app.post('/register_AirlineStaff',(req,res)=>{
+app.post('/register_airline_staff',(req,res)=>{
     //db insert
     console.log(req.body);
-    res.sendFile(path.join(__dirname+'/public/home_page.html'));
+    res.redirect('/');
+    //res.sendFile(path.join(__dirname, '/public/home_page.html'));
 })
 
+//HOME
 app.post('/',(req,res)=>{
     console.log(req.body);
     if (req.body.action==='search_for_upcoming_flights'){
@@ -80,6 +93,75 @@ app.post('/',(req,res)=>{
         //db query
         res.render('home_check_flight',{content:'test'});
     } 
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    if (req.body.action==='search_for_upcoming_flights'){
+        //db query
+        res.render('home_search_upcoming',{content:'test'});
+    } else if (req.body.action==='check_flight_status'){
+        //db query
+        res.render('home_check_flight',{content:'test'});
+    } 
+})
+
+//CUSTOMER
+app.post('/user_home_customer',(req,res)=>{
+    //todo identity check
+    if (req.body.action==="view_my_flights"){
+        console.log('testing');
+        //todo db query
+        res.render('customer_view_my_flights',{content:'test'});
+    } else if (req.body.action==="search_for_upcoming_flights"){
+        //db query
+        res.render('customer_search_upcoming',{content:'test'});
+    } else if (req.body.action==='check_flight_status'){
+        //db query
+        res.render('customer_check_flight',{content:'test'});
+    } 
+    //else if
+})
+
+// ttdd customer buy
+//app.post('/agent_search_flights',(req,res)=>{
+
+//AGENT
+app.post('/user_home_booking_agent',(req,res)=>{
+    //todo identity check
+    if (req.body.action==="search_for_upcoming_flights"){
+        //todo db query
+        res.render('agent_search_flights',{content:'test'});
+    } else if (req.body.action==='check_flight_status'){
+        //db query
+        res.render('agent_check_flight',{content:'test'});
+    } else if (req.body.action==="view_my_flights"){
+        //todo db query
+        res.render('agent_view_my_flights',{content:'test'});
+    }
+})
+
+app.post('/agent_search_flights',(req,res)=>{
+    //agent buy
+    //todo identity check
+    console.log(req.body.purchase_ticket_for_customer, req.body.select_flight, req.body.input_airline);
+    //todo db query
+    res.redirect('/user_home_booking_agent');
+})
+
+
+
+//STAFF
+app.post('/user_home_airline_staff',(req,res)=>{
+    //todo identity check
+    if (req.body.action==="search_for_upcoming_flights"){
+        //db query
+        res.render('staff_search_upcoming',{content:'test'});
+    } else if (req.body.action==='check_flight_status'){
+        //db query
+        res.render('staff_check_flight',{content:'test'});
+    } 
+    //else if
 })
 
 app.listen(3000);
