e1f24888ad0db2316d2c2b3d13b7cea6918d3f3a
Yian Zhang <yian.zhang@nyu.edu>
2020-12-17 20:22:31 +0800

    sql correction

--- db/home_search_upcoming.sql
+++ db/home_search_upcoming.sql
@@ -12,9 +12,9 @@ SET @ticket_id := UNIX_TIMESTAMP(now());
 INSERT INTO ticket VALUES(@ticket_id, '${req.body.input_airline}', ${req.body.select_flight});
 
 -- if user type = "customer":
-INSERT INTO purchase VALUES (@ticket_id, '${req.session.data.email}', NULL, NOW());
+INSERT INTO purchases VALUES (@ticket_id, '${req.session.data.email}', NULL, NOW());
 -- else if user type = "agent":
-INSERT INTO purchase VALUES (@ticket_id, '${req.body.purchase_ticket_for_customer}', '${req.session.data.booking_agent_id}', NOW());
+INSERT INTO purchases VALUES (@ticket_id, '${req.body.purchase_ticket_for_customer}', '${req.session.data.booking_agent_id}', NOW());
 -- else
 -- NULL/error/nothing happens
 
